1fa4b1407e967ad21c0d13454bf7b35cf9a7af3e
YQ0Sun <2996136822@qq.com>
2026-01-06 20:55:28 +0800

    return x + x3 + self.act1(self.bn(x4 + x5))

--- ultralytics-main/ultralytics/nn/modules/conv.py
+++ ultralytics-main/ultralytics/nn/modules/conv.py
@@ -522,7 +522,7 @@ class ChannelAttention_1(nn.Module):
 
         x4 = self.fc_3(self.fc(x))
         x5 = self.fc_5(self.fc(x))
-        return x3 + self.act1(self.bn(x4 + x5))
+        return x + x3 + self.act1(self.bn(x4 + x5))
 
 class ChannelAttentionWithSkip(nn.Module):
     """Channel-attention module with skip connection."""
